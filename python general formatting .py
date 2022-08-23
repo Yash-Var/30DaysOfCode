7a0889e2d939dd3ecf5aa5a2eddbf8498dd09ee6
Yash Varshney <yashvarshneypepsi@gmail.com>
2022-08-23 21:24:45 +0530

    output formatting

---
+++ python general formatting .py
@@ -0,0 +1,77 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+num=int(input("enter your age :"))
+
+
+# In[3]:
+
+
+print(num)
+
+
+# In[4]:
+
+
+print(type(num))
+
+
+# In[5]:
+
+
+num=10
+
+
+# In[6]:
+
+
+n=1+2+3+  4+5+6+  7+8
+
+
+# In[7]:
+
+
+print(n)
+
+
+# In[8]:
+
+
+n2=(1+2+3+
+  4+5+6+
+  7+8)
+
+
+# In[9]:
+
+
+
+print(n2)
+
+
+# # Output formatting
+
+# In[20]:
+
+
+a=10
+b=20
+print("The value of a is {} and b is {}".format(a,b)) #deafult
+
+
+# In[21]:
+
+
+a=10
+b=20
+print("The value of a is {1} and b is {0}".format(a,b)) #specified the position
+
+
+# In[ ]:
+
+
+
+
